27b1ade7e5916409c1f17d23c6805822e3ae454b
Clarencio Tan <clarenciotan@gmail.com>
2018-11-14 15:41:40 +1300

    modified to remove the unnecessary initial PO calls.

--- src/page_objects/SearchHome.ts
+++ src/page_objects/SearchHome.ts
@@ -9,31 +9,6 @@ export class SearchHome extends BasePage{
         // this will set timeouts based from BasePage
         super();
     }
-    
-
-    private getSearchInput() : WebdriverIO.Client<WebdriverIO.RawResult<WebdriverIO.Element>> {
-        return this.getTheElement(this.INPUT_ELEM);
-    }
-
-    // this method will use getSearchInput()
-    public searchForInput(inputS : string){
-        try {
-            this.getSearchInput().setValue(inputS);
-        } catch (error) {
-            console.error(`==> I can't find the "${this.INPUT_ELEM.elementName}"`);
-            throw error;
-        }
-    }
-
-    // this one won't
-    public searchForInputInd(inputS : string){
-        try {
-            this.getTheElement(this.INPUT_ELEM).setValue(inputS);
-        } catch (error) {
-            console.error(`==> I can't find the "${this.INPUT_ELEM.elementName}"`);
-            throw error;
-        }
-    }
 
     // this one is encapsulated, the better implementation.
     public searchForTheKeyword(keyword : string){
